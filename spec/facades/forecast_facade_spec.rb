7a1f5a726810124f7ee1e19897c8703e004b0cf8
Joel Grant <joelmgrant@me.com>
2022-03-04 15:14:40 -0700

    Test: Create initial facade test for forecasts

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe ForecastFacade do
+  describe '::all_weather' do
+    it 'returns all the weather from the one call api endpoint' do
+      data = ForecastFacade.all_weather("denver,co")
+
+      expect(data[:lat]).to eq(39.7385)
+      expect(data[:lon]).to eq(-104.9849)
+      expect(data[:current]).to have_key(:dt)
+      expect(data[:current]).to have_key(:sunrise)
+      expect(data[:current]).to have_key(:sunset)
+      expect(data[:current]).to have_key(:temp)
+      expect(data[:current]).to have_key(:feels_like)
+      expect(data[:current]).to have_key(:humidity)
+      expect(data[:current]).to have_key(:uvi)
+      expect(data[:current]).to have_key(:visibility)
+      expect(data[:current][:weather][0]).to have_key(:description)
+      expect(data[:current][:weather][0]).to have_key(:icon)
+
+
+      # require 'pry'; binding.pry
+    end
+  end
+end
