f47d47aa1896a24c989bcdd760810a5ba69e2d37
Joel Grant <joelmgrant@me.com>
2022-03-04 17:03:33 -0700

    Test: Create basic test for exposing forecast endpoint

--- spec/api/v1/forecast_request_spec.rb
+++ spec/api/v1/forecast_request_spec.rb
@@ -6,7 +6,40 @@ RSpec.describe 'Forecast API' do
       location = "Breckenridge"
       get "/api/v1/forecast?location=#{location}"
       something = JSON.parse(response.body, symbolize_names: true)
-      require 'pry'; binding.pry
+
+      expect(something).to have_key(:data)
+      expect(something[:data]).to have_key(:id)
+      expect(something[:data]).to have_key(:type)
+      expect(something[:data]).to have_key(:attributes)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:datetime)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:sunrise)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:sunset)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:temperature)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:feels_like)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:humidity)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:uvi)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:visibility)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:conditions)
+      expect(something[:data][:attributes][:current_weather]).to have_key(:icon)
+
+      expect(something[:data][:attributes][:daily_weather].count).to eq(5)
+      something[:data][:attributes][:daily_weather].each do |weather|
+        expect(weather).to have_key(:date)
+        expect(weather).to have_key(:sunrise)
+        expect(weather).to have_key(:sunset)
+        expect(weather).to have_key(:max_temp)
+        expect(weather).to have_key(:min_temp)
+        expect(weather).to have_key(:conditions)
+        expect(weather).to have_key(:icon)
+      end
+
+      expect(something[:data][:attributes][:hourly_weather].count).to eq(8)
+      something[:data][:attributes][:hourly_weather].each do |weather|
+        expect(weather).to have_key(:time)
+        expect(weather).to have_key(:temperature)
+        expect(weather).to have_key(:conditions)
+        expect(weather).to have_key(:icon)
+      end
     end
   end
 end
