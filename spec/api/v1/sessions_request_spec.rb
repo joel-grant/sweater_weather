ba0d433ac2df14c4b453b196be265b1ee99f0911
Joel Grant <joelmgrant@me.com>
2022-03-05 00:04:56 -0700

    Test: Add sad path testing for user login

--- spec/api/v1/sessions_request_spec.rb
+++ spec/api/v1/sessions_request_spec.rb
@@ -11,7 +11,28 @@ RSpec.describe 'User Sessions' do
       post '/api/v1/sessions', params: login_params
 
       expect(response).to be_successful
-      require 'pry'; binding.pry
+    end
+
+    it 'returns an error if the password is bad' do
+      user_params = { email: 'joe@shmo.com', password: '12345', password_confirmation: '12345'}
+      post '/api/v1/users', params: user_params
+
+      expect(response).to be_successful
+      login_params = {email: 'joe@shmo.com', password: 'ABCDE'}
+      post '/api/v1/sessions', params: login_params
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result[:error]).to eq("Your credentials are incorrect!")
+    end
+
+    it 'returns an error if the user doesnt exist' do
+      user_params = { email: 'joe@shmo.com', password: '12345', password_confirmation: '12345'}
+      post '/api/v1/users', params: user_params
+
+      expect(response).to be_successful
+      login_params = {email: 'joe12345@shmo.com', password: 'ABCDE'}
+      post '/api/v1/sessions', params: login_params
+      result = JSON.parse(response.body, symbolize_names: true)
+      expect(result[:error]).to eq("Your credentials are incorrect!")
     end
   end
 end
