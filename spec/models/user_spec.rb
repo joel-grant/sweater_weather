6b74fc508d7f1ad0ec542c75b5eed7ca78e50373
Joel Grant <joelmgrant@me.com>
2022-03-04 20:13:48 -0700

    Test: Create User validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it { should validate_presence_of(:username) }
-  it { should validate_uniqueness_of(:username) }
-  it { should validate_presence_of(:password_digest) }
-  it { should have_secure_password }
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should have_secure_password }
+
+  end
 end
