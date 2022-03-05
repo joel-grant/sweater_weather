2b117982ec2c25a900cdcd5db13d9fde9d3366be
Joel Grant <joelmgrant@me.com>
2022-03-04 23:26:07 -0700

    Test: Create tests for creating a user and api token

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,8 @@ require 'rails_helper'
 
 RSpec.describe User do
   describe 'validations' do
-    # it { should have_secure_token }
+    it { should have_secure_token(:auth_token) }
     it { should validate_presence_of(:email) }
-    # it { should validate_uniqueness_of(:token) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password_digest) }
     it { should have_secure_password }
