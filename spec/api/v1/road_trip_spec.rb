b2fa9cb598df9de7540aee3049ddd08677345022
Joel Grant <joelmgrant@me.com>
2022-03-05 20:52:34 -0700

    Test: Create sad path test for impossible routes

--- spec/api/v1/road_trip_spec.rb
+++ spec/api/v1/road_trip_spec.rb
@@ -21,5 +21,20 @@ RSpec.describe 'Road Trip API Endpoint' do
       expect(result[:data][:attributes][:weather_at_eta]).to have_key(:temperature)
       expect(result[:data][:attributes][:weather_at_eta]).to have_key(:conditions)
     end
+
+    it 'will say that a route is impossible if the locations arent connected' do
+      user_params = { email: 'joe@shmo.com', password: '12345', password_confirmation: '12345'}
+      post '/api/v1/users', params: user_params
+      last_user = User.last
+
+      trip_params = { origin: "Denver,CO", destination: "Sydney, Australia", api_key: last_user.auth_token }
+      post '/api/v1/road_trip', params: trip_params
+      result = JSON.parse(response.body, symbolize_names: true)
+
+      expect(result).to have_key(:data)
+      expect(result[:data][:attributes][:travel_time]).to eq("impossible")
+      expect(result[:data][:attributes][:weather_at_eta][:temperature]).to be nil
+      expect(result[:data][:attributes][:weather_at_eta][:conditions]).to be nil
+    end
   end
 end
