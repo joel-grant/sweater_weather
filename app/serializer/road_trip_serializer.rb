04309f992681f6302f17a1c527605232373fa699
Joel Grant <joelmgrant@me.com>
2022-03-05 20:53:19 -0700

    Create process to deal with impossible routes

--- app/serializer/road_trip_serializer.rb
+++ app/serializer/road_trip_serializer.rb
@@ -1,6 +1,17 @@
 class RoadTripSerializer
   def self.format(data, start, stop, weather)
-    trip_time = (data[:route][:time]/60/60.to_f).ceil
+    if !data[:route][:time].nil?
+      trip_time = (data[:route][:time]/60/60.to_f).ceil
+      formatted_travel_time = data[:route][:formattedTime]
+      temperature = weather[trip_time - 1][:temp]
+      conditions = weather[trip_time -1][:weather][0][:main]
+    else
+      weather = nil
+      formatted_travel_time = "impossible"
+      temperature = nil
+      conditions = nil
+    end
+
     {
       "data": {
         "id": nil,
@@ -8,10 +19,10 @@ class RoadTripSerializer
         "attributes": {
           "start_city": start,
           "end_city": stop,
-          "travel_time": data[:route][:formattedTime],
+          "travel_time": formatted_travel_time,
           "weather_at_eta": {
-            "temperature": weather[trip_time - 1][:temp],
-            "conditions": "partly cloudy with a chance of meatballs"
+            "temperature": temperature,
+            "conditions": conditions
           }
         }
       }
