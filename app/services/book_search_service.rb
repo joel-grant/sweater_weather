919de9044c7df06b37f33741e112c2a40304dbef
Joel Grant <joelmgrant@me.com>
2022-03-07 11:17:32 -0700

    Refactor Book Search Service

--- app/services/book_search_service.rb
+++ app/services/book_search_service.rb
@@ -1,6 +1,10 @@
 class BookSearchService
   def self.get_all_books(location)
-    response = Faraday.get("http://openlibrary.org/search.json?title=#{location}")
+    conn = Faraday.new(url: "http://openlibrary.org") do |faraday|
+      faraday.params[:title] = location
+    end
+
+    response = conn.get("/search.json?")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
