e507c6f3357c90b7d7849425d5397015fd8536a2
Joel Grant <joelmgrant@me.com>
2022-03-07 11:16:44 -0700

    Add additional assertions for the Book Search Service

--- spec/services/book_search_service_spec.rb
+++ spec/services/book_search_service_spec.rb
@@ -6,11 +6,26 @@ RSpec.describe BookSearchService do
       data = BookSearchService.get_all_books("denver")
 
       expect(data).to be_a Hash
-      # Make sure its parsed
+      
       expect(data).to_not be JSON
 
       expect(data[:docs][0]).to have_key(:title)
       expect(data[:docs][0][:title]).to include("Denver")
+
+      data[:docs].each do |book|
+        require 'pry'; binding.pry
+        expect(book).to have_key(:title)
+        expect(book[:title]).to be_a String
+
+        expect(book).to have_key(:publisher)
+        expect(book[:publisher]).to be_an Array
+
+
+        # ISBN Does not seem to always exist for a search result...
+
+        # expect(book).to have_key(:isbn).or eq(nil)
+        # expect(book[:isbn]).to be_an Array
+      end
     end
   end
 end
