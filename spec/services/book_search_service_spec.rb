4c3fb42aa8287e2c5bdccd684965c32ba0ba45d0
Joel Grant <joelmgrant@me.com>
2022-03-07 11:42:19 -0700

    Modify expectations since not all keys are present for each book

--- spec/services/book_search_service_spec.rb
+++ spec/services/book_search_service_spec.rb
@@ -6,19 +6,19 @@ RSpec.describe BookSearchService do
       data = BookSearchService.get_all_books("denver")
 
       expect(data).to be_a Hash
-      
+
       expect(data).to_not be JSON
 
       expect(data[:docs][0]).to have_key(:title)
       expect(data[:docs][0][:title]).to include("Denver")
 
       data[:docs].each do |book|
-        require 'pry'; binding.pry
         expect(book).to have_key(:title)
         expect(book[:title]).to be_a String
-
-        expect(book).to have_key(:publisher)
-        expect(book[:publisher]).to be_an Array
+        # require 'pry'; binding.pry
+        # Some lesser-known books also dont have a publisher field
+        # expect(book).to have_key(:publisher)
+        # expect(book[:publisher]).to be_an Array
 
 
         # ISBN Does not seem to always exist for a search result...
