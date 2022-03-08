2bba4053fc232eef7d93f2d74b15de8953a3dacf
Joel Grant <joelmgrant@me.com>
2022-03-07 22:47:44 -0700

    Add test assertion

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe ForecastFacade do
   describe '::all_weather' do
     it 'returns all the weather from the one call api endpoint' do
       data = ForecastFacade.all_weather("denver,co")
-      
+
       expect(data[:lat]).to eq(39.7385)
       expect(data[:lon]).to eq(-104.9849)
       expect(data[:current]).to have_key(:dt)
@@ -17,6 +17,7 @@ RSpec.describe ForecastFacade do
       expect(data[:current]).to have_key(:visibility)
       expect(data[:current][:weather][0]).to have_key(:main)
       expect(data[:current][:weather][0]).to have_key(:icon)
+      expect(data[:hourly].count).to eq(48)
     end
   end
 end
